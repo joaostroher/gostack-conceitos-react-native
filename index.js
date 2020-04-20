f889606ab1a7fe1a255904c10f14a2b93ec5522f
Joao Miguel Stroher <joao.stroher@gmail.com>
2020-04-19 22:20:50 -0300

    challenge done

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import { AppRegistry } from "react-native";
+import ReactNative, { AppRegistry } from "react-native";
 import App from "./src/App";
 import { name as appName } from "./app.json";
+
+ReactNative.unstable_enableLogBox();
 
 AppRegistry.registerComponent(appName, () => App);
